ebf1bc4baa4aed751f091c156e7f13bb95874aae
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2020-05-25 12:44:39 -0300

    [FIX] useDimensions initial value

--- src/useDimensions.ts
+++ src/useDimensions.ts
@@ -1,10 +1,8 @@
 import { useEffect, useState } from "react";
 import { Dimensions, ScaledSize } from "react-native";
 
-const dims = Dimensions.get("window");
-
 export default () => {
-  const [dimensions, setDimensions] = useState(dims);
+  const [dimensions, setDimensions] = useState(Dimensions.get("window"));
 
   const onChange = ({
     window: { width, height, scale, fontScale }
